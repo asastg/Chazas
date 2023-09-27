966d62b4e93c71f1ee3b32431e203f6ffe77bd24
CMChinchilla <cchinchilla@unal.edu.co>
2023-09-27 14:20:13 -0500

    Array added for admin class

--- src/main/java/entities/Admin.java
+++ src/main/java/entities/Admin.java
@@ -1,17 +1,17 @@
 package entities;
 
 public class Admin extends User{
-    private Chaza chazasAdmin;
-    public Admin(String name, String username, String password, Chaza chazasAdmin) {
+    private Chaza[] chazasAdmin;
+    public Admin(String name, String username, String password, Chaza[] chazasAdmin) {
         super(name, username, password);
         this.chazasAdmin = chazasAdmin;
     }
 
-    public Chaza getChazasAdmin() {
+    public Chaza[] getChazasAdmin() {
         return chazasAdmin;
     }
 
-    public void setChazasAdmin(Chaza chazasAdmin) {
+    public void setChazasAdmin(Chaza[] chazasAdmin) {
         this.chazasAdmin = chazasAdmin;
     }
 }
